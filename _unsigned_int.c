b297d1baef46fce87e4982167ccc8645e07d0418
Kevin Kariuki <vingentz@gmail.com>
2023-03-29 15:58:34 +0300

    Added unsigned int.h

---
+++ _unsigned_int.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * print_s_int - prints an unsigned integer
+ * @args: variadic function arguments
+ * Return: value of the return type of the integer
+ */
+int print_s_int(va_list args)
+{
+	int n;
+	int div;
+	int len;
+	unsigned int num;
+	n  = va_arg(args, int);
+	div = 1;
+	len = 0;
+
+	if (n < 0)
+	{
+		len += _writechar('-');
+		num = n * -1;
+	}
+	else
+		num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += _writechar('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+	return (len);
+}
+
+/**
+ * print_unsigned_integer - Prints an unsigned integer
+ * @arg_list: list of arguments
+ * Return: The number of characters printed
+ */
+
+int print_unsigned_integer(va_list arg_list)
+{
+	unsigned int number;
+	number = va_arg(arg_list, unsigned int);
+
+	if (number == 0)
+		return (print_u_number(number));
+	if (number < 1)
+		return (-1);
+	return (print_u_number(number));
+}
--- printf.c
+++ printf.c
@@ -1,50 +1,40 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
 
 /**
- * _printf - main entry point
- * @format: string containing all characters
- * Return: count of characters
+ * _printf - Gets main string and parameters to print formatted string
+ * @format: A string containing all the characters
+ * Return: A total character count
  */
-
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list args;
+	int printed_chars;
+	/*list of functions for different format specifiers*/
+	conver_t function_list[] = {
+			{"c", print_character},
+			{"s", print_string},
+			{"%", print_percent},
+			{"d", print_integer},
+			{"i", print_integer},
+			{"b", print_binary},
+			{"r", print_reversed},
+			{"R", rot13},
+			{"u", print_unsigned_integer},
+			{"o", print_octal},
+			{"x", print_hex_lo},
+			{"X", print_hex_cap},
+			{NULL, NULL}
+		};
 
-	va_start(args, format);
+	va_list arg_list;
+	/*checking if format string is null*/
+	if (format == NULL)
+		return (-1);
 
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					putchar(va_arg(args, int));
-					count++;
-					break;
-				case 's':
-					count += printf("%s", va_arg(args, char *));
-					break;
-				case '%':
-					putchar('%');
-					count++;
-					break;
-				default:
-					fprintf(stderr, "Error: invalid conversion specifier '%%%c'\n", *format);
-					return (-1);
-			}
-		}
-		else
-		{
-			putchar(*format);
-			count++;
-		}
-		format++;
-	}
-	va_end(args);
-	return (count);
+	/*initializing the list of arguments*/
+	va_start(arg_list, format);
+	/*Calling parser function to parse the format string and arguments*/
+	printed_chars = parser(format, function_list, arg_list);
+	/*ending the list of arguments*/
+	va_end(arg_list);
+	return (printed_chars);
 }
